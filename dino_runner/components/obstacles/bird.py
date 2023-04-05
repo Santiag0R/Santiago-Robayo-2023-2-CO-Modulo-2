7e35611021d4a17b0ff038b0566146297592f463
Santiag0R <129460446+Santiag0R@users.noreply.github.com>
2023-04-04 21:42:42 -0500

    Homework class three corrected

--- dino_runner/components/obstacles/bird.py
+++ dino_runner/components/obstacles/bird.py
@@ -1,12 +1,19 @@
+import random
 from dino_runner.components.obstacles.obstacle import Obstacle
+from dino_runner.utils.constants import BIRD
 
 
 class Bird(Obstacle):
-    def __init__(self, image, bird_frames, obstacle_type):
-        pass
+    BIRD_HEIGHTS = [200,220,170]
 
-    def update(self, game_speed):
-        pass
+    def __init__(self):
+        self.type = 0
+        super().__init__(BIRD,self.type)
+        self.rect.y = self.BIRD_HEIGHTS[random.randint(0,2)]
+        self.index = 0
 
     def draw(self, screen):
-        pass
+        if self.index >= 10:
+            self.index = 0
+        screen.blit(BIRD[self.index // 5], (self.rect.x, self.rect.y))
+        self.index += 1
--- dino_runner/components/obstacles/obstacle_manager.py
+++ dino_runner/components/obstacles/obstacle_manager.py
@@ -1,6 +1,5 @@
 import random
-import pygame
-from dino_runner.components import obstacles
+from dino_runner.components.obstacles.bird import Bird
 
 from dino_runner.components.obstacles.cactus import Cactus
 from dino_runner.utils.constants import LARGE_CACTUS, SMALL_CACTUS
@@ -10,18 +9,10 @@ class ObstacleManager:
     def __init__(self):
         self.obstacles = []
 
-    def update(self, game):
-        cactus_list = [SMALL_CACTUS,LARGE_CACTUS]
-        cactus_type = random.randint(0,1)
-        
+    def update(self, game):     
         if len(self.obstacles)==0:
-            cactus_cord_y = 0
-            if cactus_type == 0:
-                cactus_cord_y = 330
-            else:
-                cactus_cord_y = 305
-            cactus = Cactus(cactus_list[cactus_type],cactus_cord_y)
-            self.obstacles.append(cactus)
+            obstacle = self.generate_obstacle(random.randint(0,2))
+            self.obstacles.append(obstacle)
 
         for obstacle in self.obstacles:
             obstacle.update(game.game_speed, self.obstacles)
@@ -32,4 +23,14 @@ class ObstacleManager:
     def draw (self, screen):
         for obstacle in self.obstacles:  
             obstacle.draw(screen)
-            
+    
+    def generate_obstacle(self, obstacle_type):
+        if obstacle_type == 0:
+            cactus_type = "SMALL"
+            obstacle = Cactus(cactus_type)
+        elif obstacle_type == 1:
+            cactus_type = "LARGE"
+            obstacle = Cactus(cactus_type)
+        else:
+            obstacle = Bird()
+        return obstacle
