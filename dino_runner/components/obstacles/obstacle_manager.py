c0fa6bf8752b2be205d2fd01bed5bfab9abb791f
Santiag0R <129460446+Santiag0R@users.noreply.github.com>
2023-04-04 22:48:15 -0500

    Four class

--- dino_runner/components/obstacles/obstacle_manager.py
+++ dino_runner/components/obstacles/obstacle_manager.py
@@ -18,6 +18,7 @@ class ObstacleManager:
             obstacle.update(game.game_speed, self.obstacles)
             if game.player.dino_rect.colliderect(obstacle.rect):
                 game.playing = False
+                game.death_counts +=1
                 break
 
     def draw (self, screen):
@@ -34,3 +35,7 @@ class ObstacleManager:
         else:
             obstacle = Bird()
         return obstacle
+    
+    def reset_obstacles(self):
+        self.obstacles = []
+        
